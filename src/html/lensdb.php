61125991283827de1b342c779fd848fc1b9096d2
Lennard Sommer <lennard.sommer@gmx.de>
2018-02-03 19:24:47 +0100

    Added modal for lens input and calculator for ebay and paypal fees

--- src/html/lensdb.php
+++ src/html/lensdb.php
@@ -12,7 +12,8 @@
 <body>
 	<nav class="navbar navbar-dark bg-dark">
 		<span class="navbar-brand mb-0 h1">Lens DB</span>
-		<a class="btn btn-outline-light" href="index.html">Create new Entry</a>
+		<a class="btn btn-outline-light text-light" data-toggle='modal' data-target='.bd-calc-modal'>Revenue Calc</a>
+		<a class="btn btn-outline-light text-light" data-toggle='modal' data-target='.bd-lens_modal'>Create new Entry</a>
 		<!-- Navbar content -->
 	</nav>
 	<div class="container">
@@ -209,7 +210,7 @@
 
 	?>
 
-	<a class="btn btn-dark" href="index.html">Create new Entry</a>
+	<a class="btn btn-dark text-light" data-toggle='modal' data-target='.bd-lens_modal'>Create new Entry</a>
 </div>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -250,6 +251,7 @@
 		request.done(function (response, textStatus, jqXHR){
         			// Log a message to the console
         			alert('Lens edited successfully!');
+        			location.reload();
         		});
 	}
 
@@ -282,7 +284,152 @@
 		</div>
 	</div>
 </div>
+
+<div class="modal fade bd-lens_modal" tabindex="-1" role="dialog" aria-labelledby="myBigModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="myBigModalLabel">New entry</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="lens_form">
+					
+					<h2>Data <small>technical</small></h2>
+					<div class="form-group">
+						<label class="form-control-label" for="name_input">Name:</label>
+						<input class="form-control" id="name_input" type="text" name="name" placeholder="Enter name">
+					</div>
+					<div class="form-group">
+						<label class="form-control-label" for="focal_length_input">Focal Length:</label>
+						<input class="form-control" id="focal_length_input" type="text" name="focal_length" placeholder="Focal length">
+					</div>
+
+					<div class="form-group">
+						<label class="form-control-label" for="focal_input">Focal:</label>
+						<input class="form-control" id="focal_input" type="text" name="focal" placeholder="Focal">
+					</div>
+					<div class="form-group">
+						<label class="form-control-label" for="mount_input">Mount:</label>
+						<input class="form-control" id="mount_input" type="text" name="mount" placeholder="Mount">
+					</div>
+
+					<h2>Notes <small>informistical</small></h2>
+					<div class="form-group">
+						<label class="form-control-label" for="condition_input">Condition:</label>
+						<input class="form-control" id="condition_input" type="text" name="condition" placeholder="Condition">
+					</div>
+					<div class="form-group">
+						<label class="form-control-label" for="notes_input">Notes:</label>
+						<input class="form-control" id="notes_input" type="text" name="notes" placeholder="Notes">
+					</div>
+
+					<h2>Money <small>capitalistical</small></h2>
+					<div class="form-group">
+						<label class="form-control-label" for="price_in_input">Price in:</label>
+						<input class="form-control" id="price_in_input" type="text" name="price_in" placeholder="Price in">
+					</div>
+					<div class="form-group">
+						<label class="form-control-label" for="price_out_input">Price out:</label>
+						<input class="form-control" id="price_out_input" type="text" name="price_out" placeholder="Price out">
+					</div>
+
+					
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary form-control">Submit</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade bd-calc-modal" tabindex="-1" role="dialog" aria-labelledby="myCalcModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Calc Revenue</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+					<form>
+						<div class="form-group">
+							<label class="form-control-label" for="inputPrice">Price:</label>
+							<input id="inputPrice" type="text" class="form-control" aria-label="Text input with checkbox" value="0">
+						</div>
+						<div class="form-group">
+							<label class="form-control-label" for="inputShip">Shipping:</label>
+							<input id="inputShip" type="text" class="form-control" aria-label="Text input with checkbox" value="0">
+						</div>
+						<h2>Fees:</h2>
+						<div class="form-group">
+							<label class="form-control-label" for="checkPayPal">PayPal</label>
+							<input id="checkPayPal" type="checkbox" aria-label="PayPal">
+						</div>
+						<div class="form-group">
+							<label class="form-control-label" for="checkEbay">eBay</label>
+							<input id="checkEbay" type="checkbox" aria-label="eBay">
+						</div>
+
+						
+						<h1 id="calcResult">0.00</h1>
+					</form>
+			</div>
+		</div>
+	</div>
+</div>
 <script type="text/javascript">
+
+	$('#lens_form').submit(function(event) {
+		event.preventDefault();
+
+		if (request) {
+			request.abort();
+		}
+
+		var $form = $(this);
+
+		var $inputs = $form.find("input, textarea");
+
+		var serializedData = $form.serialize();
+
+		$inputs.prop("disabled", true);
+
+		console.log('start request');
+		request = $.ajax({
+			url: "post.php",
+			type: "post",
+			data: serializedData
+		});
+
+		request.done(function (response, textStatus, jqXHR){
+        			// Log a message to the console
+        			alert('Lens added!');
+        			location.reload();
+        			console.log('worked');
+        		});
+
+		request.fail(function (jqXHR, textStatus, errorThrown){
+        			// Log the error to the console
+        			console.log('failed');
+        			alert(
+        				"The following error occurred: "+
+        				jqXHR.status + " " + errorThrown
+        				);
+        		});
+
+		request.always(function () {
+        			// Reenable the inputs
+        			console.log('Nothing');
+        			$inputs.prop("disabled", false);
+        		});
+	});
+
 	$('#misc_form').submit(function(event) {
 		event.preventDefault();
 
@@ -307,7 +454,8 @@
 
 		request.done(function (response, textStatus, jqXHR){
         			// Log a message to the console
-        			alert('Added successfully');
+        			alert('Entry added!');
+        			location.reload();
         			console.log('worked');
         		});
 
@@ -326,6 +474,50 @@
         			$inputs.prop("disabled", false);
         		});
 	});
+
+	//Calc logic
+
+	$('#checkPayPal').change(function() {
+          updatePrice();
+    });
+
+    $('#checkEbay').change(function() {
+         updatePrice(); 
+    });
+
+    $('#inputPrice').change(function() {
+          updatePrice();
+    });
+
+    $('#inputShip').change(function() {
+          updatePrice();
+    });
+
+    function updatePrice(){
+
+    	var checkPP = $('#checkPayPal').is(':checked');
+    	var checkE = $('#checkEbay').is(':checked');
+
+    	var sell = parseFloat($('#inputPrice').val());
+    	var ship = parseFloat($('#inputShip').val());
+
+    	if (sell === "undefined") sell = 0.0;
+    	if (ship === "undefined") ship = 0.0;
+
+    	var result = sell
+
+    	if(checkPP) {
+    		result -= (sell + ship) * 0.019;
+    		result -= 0.35;
+    	} 
+    	if(checkE) {
+    		result -= sell * 0.1
+    	} 
+
+    	
+
+    	$("#calcResult").html(result);
+    }
 
 </script>
 </body>
